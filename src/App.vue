65e5b944dffa6f98d08052ec40d4de9bb5fc5bad
韦相龙 <gk525152097@163.com>
2018-09-19 10:38:59 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSizeReset = window.innerWidth / 10
+  fontSizeReset = fontSizeReset > 50 ? 50 : fontSizeReset
+  html.style.fontSize = fontSizeReset + 'px'
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,294 @@
+<template>
+  <div class="menu-bar">
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow" :class="{'hide-box-shadow': ifSettingShow || !ifTitleAndMenuShow}">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon" @click="ShowSetting(3)"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-equalizer icon" @click="ShowSetting(2)"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-sun icon" @click="ShowSetting(1)"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-a icon" @click="ShowSetting(0)">A</span>
+        </div>
+      </div>
+    </transition>
+    <transition name="slide-up">
+      <div class="setting-wrapper" v-show="ifSettingShow">
+        <div class="setting-font-size" v-if="showTag === 0">
+          <div class="preview" :style="{fontSize: fontSizeList[0].fontSize + 'px'}">A</div>
+          <div class="select">
+            <div class="select-wrapper" v-for="(item, index) in fontSizeList" :key="index" @click="setFontSize(item.fontSize)">
+              <div class="line"></div>
+              <div class="point-wrapper">
+                <div class="point" v-show="defaultFontSize === item.fontSize">
+                  <div class="small-point"></div>
+                </div>
+              </div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="preview" :style="{fontSize: fontSizeList[fontSizeList.length - 1].fontSize + 'px'}">A</div>
+        </div>
+        <div class="setting-theme" v-else-if="showTag === 1">
+          <div class="setting-theme-item" v-for="(item, index) in themeList" :key="index" @click="setTheme(index)">
+            <div class="preview" :style="{background: item.style.body.background}" :class="{'no-border': item.style.body.background !== '#ffffff'}"></div>
+            <div class="text" :class="{'selected': index === defaultTheme}">{{item.name}}</div>
+          </div>
+        </div>
+        <div class="setting-progress" v-else-if="showTag === 2">
+          <div class="progress-wrapper">
+            <input type="range" max="100" min="0" step="1" class="progress" @change="onProgressChange($event.target.value)" @input="onProgressInput($event.target.value)" :value="progress" :disabled="!bookAvailable" ref="progress">
+          </div>
+          <div class="text-wrapper">
+            <span>{{bookAvailable ? progress + '%' : '加载中……'}}</span>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <content-view :ifShowContent="ifShowContent" v-show="ifShowContent" :navigation="navigation" :bookAvailable="bookAvailable" @jumpTo="jumpTo"></content-view>
+    <transition name="fade">
+      <div class="content-mask" v-show="ifShowContent" @click="hideContent"></div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import ContentView from '@/components/Content'
+export default {
+  components: {
+    ContentView
+  },
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    },
+    fontSizeList: Array,
+    defaultFontSize: Number,
+    themeList: Array,
+    defaultTheme: Number,
+    bookAvailable: Boolean,
+    navigation: Object
+  },
+  data () {
+    return {
+      ifSettingShow: false,
+      showTag: 0,
+      progress: 0,
+      ifShowContent: false
+    }
+  },
+  methods: {
+    hideContent() {
+      this.ifShowContent = false
+    },
+    jumpTo(target) {
+      this.$emit('jumpTo', target)
+    },
+    onProgressInput(progress) {
+      this.progress = progress
+      this.$refs.progress.style.backgroundSize = `${this.progress}% 100%`
+    },
+    onProgressChange(progress) {
+      this.$emit('onProgressChange', progress)
+    },
+    setTheme(index) {
+      this.$emit('setTheme', index)
+    },
+    setFontSize(fontSize) {
+      this.$emit('setFontSize', fontSize)
+    },
+    ShowSetting(tag) {
+      this.showTag = tag
+      if (this.showTag === 3) {
+        this.ifSettingShow = false
+        this.ifShowContent = true
+      } else {
+        this.ifSettingShow = true
+      }
+    },
+    hideSetting() {
+      this.ifSettingShow = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/global.scss";
+.menu-bar {
+  .menu-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 101;
+    width: 100%;
+    height: px2rem(48);
+    background: #ffffff;
+    display: flex;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    &.hide-box-shadow{
+      box-shadow: none;
+    }
+    .icon-wrapper{
+      flex: 1;
+      @include center;
+      align-items: center;
+    }
+  }
+  .setting-wrapper {
+    position: absolute;
+    bottom: px2rem(48);
+    left: 0;
+    z-index: 101;
+    width: 100%;
+    height: px2rem(60);
+    background: #ffffff;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    .setting-font-size {
+      display: flex;
+      height: 100%;
+      align-items: center;
+      .preview {
+        flex: 0 0 px2rem(40);
+        @include center
+      }
+      .select{
+        display: flex;
+        flex: 1;
+        .select-wrapper {
+          flex: 1;
+          display: flex;
+          &:first-child {
+            .line {
+              &:first-child {
+                border-top:none;
+              }
+            }
+          }
+          &:last-child {
+            .line {
+              &:last-child {
+                border-top:none;
+              }
+            }
+          }
+          .line {
+            flex: 1;
+            height: 0;
+            border-top: px2rem(1) solid #cccccc ;
+          }
+          .point-wrapper {
+            position: relative;
+            flex: 0 0 0;
+            width: 0;
+            height: px2rem(7);
+            border-left: px2rem(1) solid #cccccc;
+            .point{
+              position: absolute;
+              top: px2rem(-10);
+              left: px2rem(-10);
+              width: px2rem(20);
+              height: px2rem(20);
+              background: #ffffff;
+              border-radius: 50%;
+              border: px2rem(1) solid #cccccc;
+              box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, .15);
+              @include center;
+              .small-point {
+                width: px2rem(5);
+                height: px2rem(5);
+                background: #000000;
+                border-radius: 50%;
+              }
+            }
+          }
+        }
+      }
+    };
+    .setting-theme {
+      display: flex;
+      height: 100%;
+      .setting-theme-item {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: px2rem(4);
+        box-sizing: border-box;
+        .preview{
+          flex: 1;
+          border: px2rem(1) solid #cccccc;
+          box-sizing: border-box;
+          &.no-border {
+            border: none;
+          }
+        }
+        .text{
+          flex: 0 0 px2rem(20);
+          font-size: px2rem(14);
+          color: #cccccc;
+          @include center;
+          &.selected {
+            color: #000000;
+          }
+        }
+      }
+    };
+    .setting-progress{
+      position: relative;
+      width: 100%;
+      height: 100%;
+      .progress-wrapper {
+        width: 100%;
+        height: 100%;
+        @include center;
+        padding: 0 px2rem(30);
+        box-sizing: border-box;
+        .progress {
+          width: 100%;
+          /*覆盖默认样式*/
+          -webkit-appearance: none;
+          height: px2rem(2);
+          background: -webkit-linear-gradient(#999999, #999999) no-repeat #dddddd;
+          background-size: 0 100%;
+          &:focus {
+            outline: none;
+          }
+          &::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            height: px2rem(20);
+            width: px2rem(20);
+            border-radius: 50%;
+            background: #ffffff;
+            box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .15);
+            border: px2rem(1) solid #dddddd;
+          }
+        };
+      }
+      .text-wrapper{
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        color: #333333;
+        font-size: px2rem(12);
+        text-align: center;
+      }
+    }
+  }
+  .content-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background: rgba(51, 51, 51, .8);
+  }
+}
+</style>
